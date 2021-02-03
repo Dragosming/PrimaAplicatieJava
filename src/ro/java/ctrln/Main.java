2fa68118260e6b813ee8381b7170a64f622ee5e2
Dragosming <dragos_mingheras@yahoo.com>
2021-02-03 15:45:08 +0200

    added single line comms

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,7 +2,9 @@ package ro.java.ctrln;
 
 public class Main {
 
+    // Aceasta este metoda main care porneste o aplicatie java
     public static void main(String[] args) {
+        // aici vrem sa afisam pe consola intellij un mesaj ales de noi
         System.out.println("Hello world!");
     }
 }
